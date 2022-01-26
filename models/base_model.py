5369d2e2fb0e0b9529d3e09ac674726c3f2864b4
Noah Chernet <noahchernet@yahoo.com>
2022-01-26 11:18:22 +0300

    Done first task of this project, made the BaseModel class in base_model.py and a unittest for it in tests/test_base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+This module contains the class BaseModel. Description of th class can be found in the class's docstring
+"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    This class defines all common attributes/methods for other classes
+    """
+    def __init__(self):
+        """
+        Initializes a BaseModel object
+        """
+        self.id = str(uuid4())  # Generate a unique ID for the instance
+        self.created_at = datetime.now()  # Save the time the object was created
+        # Updated every time the object is updated. Initialized to current time when object is created
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Overriding the __str__ method to display a custom string representation"""
+        print("{} ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return "{} ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates @updated_at to the current date & time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        dict_repr = {}
+        dict_repr.update(self.__dict__)
+        dict_repr.update([("__class__", self.__class__.__name__), ("created_at", self.created_at.isoformat()),
+                          ("updated_at", self.updated_at.isoformat())])
+        return dict_repr
